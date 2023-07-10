654c5dbd74f107e55139c2d1a80a20b71286102f
Grayson Wang <1183210702@qq.com>
2023-07-10 23:23:32 +0800

    fundemantal win64 Qt framework

---
+++ src/build-Player-Desktop_Qt_6_4_2_MSVC2019_64bit-Debug/qtcsettings.cmake
@@ -0,0 +1,9 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "E:/Qt/6.4.2/msvc2019_64/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "E:/Qt/6.4.2/msvc2019_64" CACHE "PATH" "" FORCE)
+set("CMAKE_C_COMPILER" "C:/Program Files/Microsoft Visual Studio/2022/Professional/VC/Tools/MSVC/14.36.32532/bin/HostX64/x64/cl.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "C:/Program Files/Microsoft Visual Studio/2022/Professional/VC/Tools/MSVC/14.36.32532/bin/HostX64/x64/cl.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
